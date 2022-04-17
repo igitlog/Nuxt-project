a2ffeca510bc2506b8c43481ce5f3334bc49e7a0
Serge K <88474455+igitlog@users.noreply.github.com>
2022-04-17 21:40:29 +0700

    Add first calculate functional

--- components/pages/FinanceCalculator/index.vue
+++ components/pages/FinanceCalculator/index.vue
@@ -6,44 +6,48 @@
         <h5>Доходы</h5>
         <label>
           Зарплата
-          <input type="number" id="" class="input" />
+          <input type="number" v-model="salary" class="input" />
         </label>
         <label>
           Фриланс
-          <input type="number" id="" class="input" />
+          <input type="number" v-model="freelance" class="input" />
         </label>
         <label>
           Доп. источники
-          <input type="number" id="" class="input" />
+          <input type="number" v-model="dopFinance" class="input" />
         </label>
       </div>
       <div class="inputBLock">
         <h5>Расходы</h5>
         <label>
           Обязательные платежи
-          <input type="number" id="" class="input" />
+          <input type="number" v-model="payments" class="input" />
         </label>
         <label>
           Продукты
-          <input type="number" id="" class="input" />
+          <input type="number" v-model="products" class="input" />
         </label>
         <label>
           Доп. траты
-          <input type="number" id="" class="input" />
+          <input type="number" v-model="dopPayments" class="input" />
         </label>
       </div>
       <div class="inputBLockTotal">
         <div class="total">
           <h5>Доступно в месяц</h5>
-          <input class="total-input" type="number" value="0" id="total-month" />
+          <input class="total-input" type="number" disabled value="totalMonthCalc" v-model="totalMonth"/>
+          <span class="hidden">{{totalMonthCalc}}</span>
         </div>
         <div class="total">
           <h5>Доступно в день</h5>
-          <input class="total-input" type="number" value="0" id="total-day" />
+          <input class="total-input" type="number" disabled value="totalDayCalc" v-model="totalDay" />
+          <span class="hidden">{{totalDayCalc}}</span>
         </div>
         <div class="total">
           <h5>Накоплю за год</h5>
-          <input class="total-input" type="number" value="0" id="kopilka" />
+          <input class="total-input" type="number" disabled value="kopilkaCalc" v-model="kopilka" />
+          <span class="hidden">{{kopilkaCalc}}</span>
+
         </div>
       </div>
     </div>
@@ -52,16 +56,57 @@
 
 <script lang="ts">
 import Vue from "vue";
-
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      salary: '' as any,
+      freelance: '' as any,
+      dopFinance: '' as any,
+      payments: '' as any,
+      products: '' as any,
+      dopPayments: '' as any,
+      totalMonth: 0,
+      totalDay: 0,
+      kopilka: 0,
+    };
+  },
+  computed: {
+    totalMonthCalc(): any {
+      const salary = Number(this.salary) + Number(this.freelance) + Number(this.dopFinance)
+      const payment = Number(this.payments) + Number(this.products) + Number(this.dopPayments)
+      if (salary || payment) {
+        return this.totalMonth = salary - payment
+      }
+      return this.totalMonth = 0
+    },
+    totalDayCalc(): any {
+      const day = Math.round(this.totalMonth / 30)
+        return this.totalDay = day
+    },
+    kopilkaCalc(): any {
+      const year = Math.round(this.totalMonth * 12)
+        return this.kopilka = year
+    }
+  },
+});
 </script>
 
 <style scoped>
 .input {
   width: 100%;
 }
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+-webkit-appearance: none; margin:0;}
+.hidden {
+  display: none;
+}
 .total-input {
+  background: #ffffff;
   width: 100%;
+}
+.total {
+  max-width: 250px;
 }
 .container {
   padding: 20px;
@@ -73,6 +118,8 @@ export default Vue.extend({});
 .inputBLock {
   padding: 15px;
   margin-right: 20px;
+  max-width: 200px;
+
 }
 .inputBLock input {
   margin: 10px;
@@ -98,4 +145,5 @@ h5 {
   outline: 0;
   font-size: 36px;
 }
+
 </style>
